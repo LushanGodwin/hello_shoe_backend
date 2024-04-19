6b2bd18f393e5b320780bb57b518cc65a6e011f4
Your Name <you@example.com>
2024-04-19 15:42:14 +0530

    initialize the project

---
+++ src/main/java/lk/ijse/helloshoesbackend/HelloShoesBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.helloshoesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloShoesBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloShoesBackEndApplication.class, args);
+    }
+
+}
