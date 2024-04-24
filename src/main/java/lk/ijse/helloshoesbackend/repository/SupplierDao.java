ab99c616e3c018b0312d14879b83716f5055110d
Your Name <you@example.com>
2024-04-25 00:23:23 +0530

    few changes

--- src/main/java/lk/ijse/helloshoesbackend/repository/SupplierDao.java
+++ src/main/java/lk/ijse/helloshoesbackend/repository/SupplierDao.java
@@ -1,4 +1,9 @@
 package lk.ijse.helloshoesbackend.repository;
 
-public interface SupplierDao {
+import lk.ijse.helloshoesbackend.entity.SupplierEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierDao extends JpaRepository<SupplierEntity,String> {
 }
