cd4124e48adb1c3504b154ec69059856b44e4277
Your Name <you@example.com>
2024-04-24 15:05:39 +0530

    update CustomerDTO

--- src/main/java/lk/ijse/helloshoesbackend/dto/CustomerDTO.java
+++ src/main/java/lk/ijse/helloshoesbackend/dto/CustomerDTO.java
@@ -1,4 +1,31 @@
 package lk.ijse.helloshoesbackend.dto;
 
-public class CustomerDTO {
+import lk.ijse.helloshoesbackend.entity.Gender;
+import lk.ijse.helloshoesbackend.entity.Level;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO implements SuperDTO{
+    private String customer_code;
+    private String customer_name;
+    private Gender gender;
+    private Date joinDate;
+    private Level level;
+    private Integer totalPoints;
+    private Date dob;
+    private String address_line_01;
+    private String address_line_02;
+    private String address_line_03;
+    private String address_line_04;
+    private String address_line_05;
+    private String contact;
+    private String email;
+    private Timestamp purchase_time_date;
 }
