67f3eb84cce4a369764ea07ee742ac35350d99f6
Your Name <you@example.com>
2024-04-30 13:56:51 +0530

    few changes

--- src/main/java/lk/ijse/helloshoesbackend/controller/User.java
+++ src/main/java/lk/ijse/helloshoesbackend/controller/User.java
@@ -1,4 +1,14 @@
 package lk.ijse.helloshoesbackend.controller;
 
+import lk.ijse.helloshoesbackend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/user")
 public class User {
+
 }
